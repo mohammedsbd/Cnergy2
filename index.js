2c0783ad95ae8d2544e577b5e40e3bbc9d911401
mohammedsbd <mohammed1112ok@gmail.com>
2025-10-13 09:16:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -48912,6 +48912,32 @@ function getRandomColor() {
   return color;
   return color;
 }
+ 
+
+setInterval(() => {
+setInterval(() => {
+setInterval(() => {
+setInterval(() => {
+setInterval(() => {
+setInterval(() => {
+  document.body.style.backgroundColor = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
+  document.body.style.backgroundColor = getRandomColor();
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+// Change the background color every second to a random color
+// Change the background color every second to a random color
+// Change the background color every second to a random color
+// Change the background color every second to a random color
+// Change the background color every second to a random color
+// Change the background color every second to a random color
 function getRandomColor() {
 function getRandomColor() {
 function getRandomColor() {
@@ -48950,31 +48976,56 @@ function getRandomColor() {
   return color;
   return color;
 }
-
-setInterval(() => {
-setInterval(() => {
-setInterval(() => {
-setInterval(() => {
-setInterval(() => {
-setInterval(() => {
-  document.body.style.backgroundColor = getRandomColor();
-  document.body.style.backgroundColor = getRandomColor();
-  document.body.style.backgroundColor = getRandomColor();
-  document.body.style.backgroundColor = getRandomColor();
-  document.body.style.backgroundColor = getRandomColor();
-  document.body.style.backgroundColor = getRandomColor();
 }, 1000);
 }, 1000);
 }, 1000);
 }, 1000);
 }, 1000);
 }, 1000);
-// Change the background color every second to a random color
-// Change the background color every second to a random color
-// Change the background color every second to a random color
-// Change the background color every second to a random color
-// Change the background color every second to a random color
-// Change the background color every second to a random color
+function getRandomColor() {
+function getRandomColor() {
+function getRandomColor() {
+function getRandomColor() {
+function getRandomColor() {
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  let color = "#";
+  let color = "#";
+  let color = "#";
+  let color = "#";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+  return color;
+  return color;
+  return color;
+  return color;
+  return color;
+}
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
 function getRandomColor() {
 function getRandomColor() {
 function getRandomColor() {
