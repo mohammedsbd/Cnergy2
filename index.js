75dc1579e6d830a476d7b892db842cdff51cf93b
mohammedsbd <mohammed1112ok@gmail.com>
2025-10-13 11:08:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -51552,6 +51552,47 @@ function getRandomColor() {
   for (let i = 0; i < 6; i++) {
   for (let i = 0; i < 6; i++) {
   for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+  return color;
+  return color;
+  return color;
+  return color;
+  return color;
+}
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  let color = "#";
+  let color = "#";
+  let color = "#";
+  let color = "#";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
     color += letters[Math.floor(Math.random() * 16)];
     color += letters[Math.floor(Math.random() * 16)];
