a2fb2438e2144cd30b380b912e6daab321978e26
mohammedsbd <mohammed1112ok@gmail.com>
2025-10-24 10:47:19 -0700

    Update new.php

--- new.php
+++ new.php
@@ -22438,6 +22438,47 @@ function getRandomColor() {
   let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
+  return color;
+}
+}
+// Change the background color every second to a random color
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+// Change the background color every second to a random color
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+}
+// Change the background color every second to a random color
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+setInterval(() => {
+  document.body.style.backgroundColor = getRandomColor();
+}, 1000);
+// Change the background color every second to a random color
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
